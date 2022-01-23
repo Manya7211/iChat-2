a8f2b1bcfeea42dd6cdf1297eb7b67e5b6a086c2
Manya7211 <83996642+Manya7211@users.noreply.github.com>
2022-01-23 06:48:56 +0530

    Add files via upload

---
+++ iChat_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCAnsy0BdOBZjFMpf5vNuz7xFRqh3GVWY8",
+    authDomain: "kwitter-38a2c.firebaseapp.com",
+    databaseURL: "https://kwitter-38a2c-default-rtdb.firebaseio.com",
+    projectId: "kwitter-38a2c",
+    storageBucket: "kwitter-38a2c.appspot.com",
+    messagingSenderId: "211002307720",
+    appId: "1:211002307720:web:31377b92112d4b90281b3d"
+};
+firebase.initializeApp(firebaseConfig);
